e4a7b6a6705d2ab93e98a2352d2ddc74089d627f
JaynouOliver <jyb2084.usa@gmail.com>
2025-03-22 05:37:33 +0530

    successfully fetched commit data

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ github_token = os.getenv('GITHUB_API_KEY')
 
 release_url = f"https://api.github.com/repos/{owner}/{repo}/releases"
 
+
 headers = {
     "Accept": "application/vnd.github+json",
     "Authorization": f"Bearer {github_token}",
@@ -23,6 +24,7 @@ def get_release_dates():
         getrequest = requests.get(release_url, headers=headers)
         getrequest.raise_for_status #this is for raising an http error if it occurs 
         data = getrequest.json()
+        # print(data)
 
         #extract the start and end dates
         get_date = [find["created_at"] for find in data]
@@ -35,10 +37,32 @@ def get_release_dates():
 
 
 
-call_function = get_release_dates()
-print(call_function)
+# call_function = get_release_dates()
+# print(call_function)
 
-start_date, end_date = call_function
+# start_date, end_date = call_function
+# print(f"{start_date} \n {end_date}")
 
+
+
+
+commit_url = f"https://api.github.com/repos/{owner}/{repo}/commits?since=2025-03-18T11:15:09Z&until=2025-03-21T13:25:38Z&per_page=100&page=1"
+
+def get_commits():
+    try:
+        getrequest = requests.get(commit_url, headers=headers)
+        getrequest.raise_for_status() #this is for raising an http error if it occurs 
+        data = getrequest.json()
+        # print(data)
+
+        get_commit = [find["commit"]["message"] for find in data]
+        return get_commit
+
+    except requests.exceptions.RequestException as e:
+        print(f"error{e}")
+
+call_func = get_commits()
+json_data = json.dumps(call_func, indent=6)
+print(json_data)
 
 
